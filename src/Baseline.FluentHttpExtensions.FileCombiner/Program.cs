a4b392b2198b6b8e9e8438dd44702e05e36cfaf6
Liam Symonds <liam@liamsymonds.com>
2021-01-31 21:50:15 +0000

    #36 - Convert project to .NET Core App 3.1

--- src/Baseline.FluentHttpExtensions.FileCombiner/Program.cs
+++ src/Baseline.FluentHttpExtensions.FileCombiner/Program.cs
@@ -2,59 +2,69 @@
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
-var usingStatements = new List<string>();
-var filesLessUsings = new List<string>();
-var filesToCombine = Directory.EnumerateFiles("../Baseline.FluentHttpExtensions/")
-    .Where(x => x.EndsWith(".cs"))
-    .OrderBy(x => x.Contains("HttpRequest.cs") ? 0 : 1)
-    .ThenBy(x => x)
-    .ToList();
-
-foreach (var file in filesToCombine)
+namespace Baseline.FluentHttpExtensions.FileCombiner
 {
-    var fileContents = await File.ReadAllLinesAsync(file);
-    var fileContentsLessUselessLines = new StringBuilder();
-    foreach (var line in fileContents)
+    public class Program
     {
-        if (line.StartsWith("using"))
+        public static async Task Main()
         {
-            if (!usingStatements.Contains(line))
+            var usingStatements = new List<string>();
+            var filesLessUsings = new List<string>();
+            var filesToCombine = Directory.EnumerateFiles("../Baseline.FluentHttpExtensions/")
+                .Where(x => x.EndsWith(".cs"))
+                .OrderBy(x => x.Contains("HttpRequest.cs") ? 0 : 1)
+                .ThenBy(x => x)
+                .ToList();
+
+            foreach (var file in filesToCombine)
             {
-                usingStatements.Add(line);
+                var fileContents = await File.ReadAllLinesAsync(file);
+                var fileContentsLessUselessLines = new StringBuilder();
+                foreach (var line in fileContents)
+                {
+                    if (line.StartsWith("using"))
+                    {
+                        if (!usingStatements.Contains(line))
+                        {
+                            usingStatements.Add(line);
+                        }
+                        continue;
+                    }
+
+                    if (string.IsNullOrWhiteSpace(line))
+                    {
+                        continue;
+                    }
+
+                    fileContentsLessUselessLines.AppendLine(line);
+                }
+                filesLessUsings.Add(fileContentsLessUselessLines.ToString());
             }
-            continue;
-        }
 
-        if (string.IsNullOrWhiteSpace(line))
-        {
-            continue;
-        }
+            var combinedFileContents = new StringBuilder();
 
-        fileContentsLessUselessLines.AppendLine(line);
+            combinedFileContents.AppendLine("// -------------------------------------------------------------------------------------------------- //");
+            combinedFileContents.AppendLine("// THIS IS A SUPER DUPER AUTOMAGICALLY GENERATED FILE. DO NOT EDIT DIRECTLY PLEASE.                   //");
+            combinedFileContents.AppendLine("// -------------------------------------------------------------------------------------------------- //");
+            combinedFileContents.AppendLine("// For more information and documentation, visit the Baseline.FluentHttpExtensions Github repository. //");                                        //");
+            combinedFileContents.AppendLine("// https://github.com/lsymonds/Baseline.FluentHttpExtensions                                          //");
+            combinedFileContents.AppendLine("// -------------------------------------------------------------------------------------------------- //\n");
+
+            foreach (var usingStatement in usingStatements.OrderBy(x => x))
+            {
+                combinedFileContents.AppendLine(usingStatement);
+            }
+
+            combinedFileContents.AppendLine("");
+
+            foreach (var fileLessUsings in filesLessUsings)
+            {
+                combinedFileContents.AppendLine(fileLessUsings);
+            }
+
+            await File.WriteAllTextAsync("../../BaselineFluentHttpExtensions.cs", combinedFileContents.ToString());
+        }
     }
-    filesLessUsings.Add(fileContentsLessUselessLines.ToString());
 }
-
-var combinedFileContents = new StringBuilder();
-
-combinedFileContents.AppendLine("// -------------------------------------------------------------------------------------------------- //");
-combinedFileContents.AppendLine("// THIS IS A SUPER DUPER AUTOMAGICALLY GENERATED FILE. DO NOT EDIT DIRECTLY PLEASE.                   //");
-combinedFileContents.AppendLine("// -------------------------------------------------------------------------------------------------- //");
-combinedFileContents.AppendLine("// For more information and documentation, visit the Baseline.FluentHttpExtensions Github repository. //");                                        //");
-combinedFileContents.AppendLine("// https://github.com/lsymonds/Baseline.FluentHttpExtensions                                          //");
-combinedFileContents.AppendLine("// -------------------------------------------------------------------------------------------------- //\n");
-
-foreach (var usingStatement in usingStatements.OrderBy(x => x))
-{
-    combinedFileContents.AppendLine(usingStatement);
-}
-
-combinedFileContents.AppendLine("");
-
-foreach (var fileLessUsings in filesLessUsings)
-{
-    combinedFileContents.AppendLine(fileLessUsings);
-}
-
-await File.WriteAllTextAsync("../../BaselineFluentHttpExtensions.cs", combinedFileContents.ToString());
